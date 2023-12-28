8494401fe5d5fb8453d6e173f3956e528431d162
gkitm1234 <155112031+gkitm1234@users.noreply.github.com>
2023-12-28 22:12:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const imagePaths = ['images/zdjecie1.jpg', 'images/zdjecie5.jpg', 'images/zdjecie2.jpg', 'images/zdjecie3.jpg', 'images/zdjecie4.jpg'];
+let currentImageIndex = 0;
+
+function changeImage() {
+    const image = document.getElementById('image');
+
+    // Zmiana obrazu na kolejny w tablicy
+    image.src = imagePaths[currentImageIndex];
+
+    // Zwiększenie indeksu, jeśli to ostatni obraz, wróć do pierwszego
+    currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
+}
+
+function moveButton() {
+    const button = document.getElementById('no-button');
+    // Tutaj dodaj kod przesuwania przycisku "Nie" po kliknięciu
+    button.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
+    button.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
+}
+
+function createSnow() {
+    const numberOfSnowflakes = 100;
+    const snowContainer = document.getElementById('snow-container');
+
+    for (let i = 0; i < numberOfSnowflakes; i++) {
+        createSnowflake(snowContainer);
+    }
+}
+
+function createSnowflake(container) {
+    const snowflake = document.createElement('div');
+    snowflake.className = 'snowflake';
+    container.appendChild(snowflake);
+
+    const randomX = Math.random() * window.innerWidth;
+    const randomY = Math.random() * window.innerHeight;
+    const randomAnimationDuration = 5 + Math.random() * 5;
+
+    snowflake.style.left = randomX + 'px';
+    snowflake.style.top = randomY + 'px';
+    snowflake.style.animationDuration = randomAnimationDuration + 's';
+
+    snowflake.addEventListener('animationend', () => {
+        snowflake.remove();
+    });
+}
